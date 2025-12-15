04be34c64f0e8dad7ff5881f7827ffd34fedaa63
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2025-12-15 13:30:00 -0300

    feat(bytecode): add u8 convertion for types

--- crates/bytecode/src/types.rs
+++ crates/bytecode/src/types.rs
@@ -1,14 +1,17 @@
 use std::fmt::Display;
 
+use thiserror::Error;
+
 use crate::values::Value;
 
+/// Hexadecimals with this template are Types 0x2_
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Type {
-    Int,
-    Float,
-    Bool,
-    Str,
-    Char,
+    Int = 0x20,
+    Float = 0x21,
+    Bool = 0x22,
+    Str = 0x23,
+    Char = 0x24,
 }
 
 impl From<Value> for Type {
@@ -23,6 +26,21 @@ impl From<Value> for Type {
     }
 }
 
+impl TryFrom<u8> for Type {
+    type Error = TypeError;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0x20 => Ok(Type::Int),
+            0x21 => Ok(Type::Float),
+            0x22 => Ok(Type::Bool),
+            0x23 => Ok(Type::Str),
+            0x24 => Ok(Type::Char),
+            _ => Err(TypeError::InvalidType(value)),
+        }
+    }
+}
+
 impl Display for Type {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
@@ -33,4 +51,10 @@ impl Display for Type {
             Type::Char => write!(f, "Char"),
         }
     }
+}
+
+#[derive(Debug, Error)]
+pub enum TypeError {
+    #[error("Invalid Type: {0}")]
+    InvalidType(u8),
 }
